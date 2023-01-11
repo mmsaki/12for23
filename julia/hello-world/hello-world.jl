d0b9af8dc3d4563f57ace2fd3b14ef5591559a34
Meek <meek10x@gmail.com>
2023-01-11 17:45:02 -0600

    one linern and function method

--- julia/hello-world/hello-world.jl
+++ julia/hello-world/hello-world.jl
@@ -1,1 +1,13 @@
-hello() = "Hello, World!"
+# One liner function
+h() = "Hello, World!"
+
+# Dubug using @show
+@show uppercase("Hello, World!")
+
+# You can also use println or any other method that writes output to stdout
+println("Hello World!")
+
+# Function
+function hello()
+    return (@show "Hello, World!")
+end
